6c673273a9596c73064742ad0a100806bc6d629a
miguel.carlos <miguel.carlos@vallegrande.edu.pe>
2024-12-21 21:39:42 +0000

    update js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Simulación de la escritura palabra por palabra
 document.addEventListener("DOMContentLoaded", () => {
-    const typingElement = document.querySelector(".typing-text");
-    const text = ["Desarrollador Web", "Diseñador", "Freelancer"];
-    let i = 0;
-  
-    function typeWord() {
-      if (i < text.length) {
-        typingElement.textContent = "";
-        let charIndex = 0;
-        function typeLetter() {
-          if (charIndex < text[i].length) {
-            typingElement.textContent += text[i].charAt(charIndex);
-            charIndex++;
-            setTimeout(typeLetter, 100);
-          } else {
-            i++;
-            setTimeout(typeWord, 1500);  // Espera entre palabras
+  const typingElement = document.querySelector(".typing-text");
+  const text = ["Desarrollador Web", "Diseñador", "Freelancer"];
+  let i = 0;
+
+  function typeWord() {
+    if (i < text.length) {
+      typingElement.textContent = "";
+      let charIndex = 0;
+      function typeLetter() {
+        if (charIndex < text[i].length) {
+          typingElement.textContent += text[i].charAt(charIndex);
+          charIndex++;
+          setTimeout(typeLetter, 100);  // Velocidad de escritura
+        } else {
+          i++;
+          if (i === text.length) {
+            i = 0;  // Reinicia el índice para comenzar nuevamente
           }
+          setTimeout(typeWord, 1500);  // Espera entre palabras
         }
-        typeLetter();
       }
+      typeLetter();
     }
-  
-    typeWord();
-  });
-  
+  }
+
+  typeWord();
+});
