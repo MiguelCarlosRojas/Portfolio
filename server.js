07a3c4561df1d1ffdb752eed3fdb808443e77e54
MiguelCarlosRojas <miguel.carlos@vallegrande.edu.pe>
2025-02-01 18:10:35 -0500

    update js

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const translate = require("google-translate-api-x");
-
 const app = express();
-const port = 3000;
+
+// Puerto dinámico (compatible con Vercel)
+const port = process.env.PORT || 3000;
 
 // Middleware para parsear JSON
 app.use(express.json());
 
-// Configurar CORS para permitir solicitudes desde cualquier origen
+// Configurar CORS para permitir solicitudes desde la URL del frontend
 app.use(
   cors({
-    origin: "*",
+    origin: "https://portfolio-miguel-carloss-projects.vercel.app", // URL del frontend
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
@@ -24,7 +25,6 @@ app.use(express.static(path.join(__dirname)));
 // Endpoint para traducir texto
 app.post("/translate", async (req, res) => {
   const { text, targetLanguage } = req.body;
-
   try {
     const result = await translate(text, { to: targetLanguage });
     res.json({ translatedText: result.text });
