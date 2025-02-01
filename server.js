4ddf4bd8d866b327d55e5dad7320d7db7e1174f7
MiguelCarlosRojas <miguel.carlos@vallegrande.edu.pe>
2025-02-01 17:47:06 -0500

    update js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors'); // Importa el middleware CORS
-const translate = require('google-translate-api-x');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const translate = require("google-translate-api-x");
 
 const app = express();
 const port = 3000;
@@ -10,29 +10,31 @@ const port = 3000;
 app.use(express.json());
 
 // Configurar CORS para permitir solicitudes desde cualquier origen
-app.use(cors({
-    origin: '*', // Permite solicitudes desde cualquier origen
-    methods: ['GET', 'POST'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type'], // Encabezados permitidos
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // Servir archivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
 
 // Endpoint para traducir texto
-app.post('/translate', async (req, res) => {
-    const { text, targetLanguage } = req.body;
+app.post("/translate", async (req, res) => {
+  const { text, targetLanguage } = req.body;
 
-    try {
-        const result = await translate(text, { to: targetLanguage });
-        res.json({ translatedText: result.text });
-    } catch (error) {
-        console.error(`Error durante la traducción: ${error.message}`);
-        res.status(500).json({ error: 'No se pudo completar la traducción.' });
-    }
+  try {
+    const result = await translate(text, { to: targetLanguage });
+    res.json({ translatedText: result.text });
+  } catch (error) {
+    console.error(`Error durante la traducción: ${error.message}`);
+    res.status(500).json({ error: "No se pudo completar la traducción." });
+  }
 });
 
 // Iniciar el servidor
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
