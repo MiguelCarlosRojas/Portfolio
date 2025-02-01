ff414bab6ee70b17f3f373f82d9dead6a88e4ca6
MiguelCarlosRojas <miguel.carlos@vallegrande.edu.pe>
2025-02-01 10:46:02 -0500

    update css

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-import { typeEffect } from './typingEffect.js';
-import { toggleMenu } from './menuToggle.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-  typeEffect();
-  toggleMenu();
-});
---
+++ src/script.js
@@ -0,0 +1,7 @@
+import { typeEffect } from './js/typingEffect.js';
+import { toggleMenu } from './js/menuToggle.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeEffect();
+  toggleMenu();
+});
