e5c20e1719d5d6d60d27a026ffff0dcf9056d076
MiguelCarlosRojas <isakiangel6@gmail.com>
2025-03-08 13:38:08 -0500

    Initial commit

--- src/app/app.js
+++
@@ -1,5 +0,0 @@
-// Importa la función initializeApp desde el archivo script.js
-import { initializeApp } from '../main.js';
-
-// Inicializa la aplicación
-initializeApp();
--- src/components/js/menuToggle.js
+++
@@ -1,6 +0,0 @@
-export function toggleMenu() {
-  document.getElementById("menu-icon").addEventListener("click", function () {
-    const navLeft = document.getElementById("nav-left");
-    navLeft.classList.toggle("active");
-  });
-}
